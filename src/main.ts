2e942c2cf10b4b08e3a79237bbb012b0af9ea9bf
Anandhu1605 <118506014+Anandhu1605@users.noreply.github.com>
2024-12-15 17:59:47 +0530

    Updated deployed URL

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', // Allow only the front-end's origin
+    origin: ['http://localhost:3000', 'https://dashboard-app-opal.vercel.app'], // Allow only the front-end's origin
     methods: 'GET, POST, PUT, DELETE', // Allow specific HTTP methods
     allowedHeaders: 'Content-Type, Authorization', // Allow specific headers
   });
